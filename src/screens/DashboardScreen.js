50891cf85f6d39628f1f8a33aa321f4a04071973
Liam <lon.liam.li@gmail.com>
2020-10-03 12:32:36 -0700

    update homescreen

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -15,18 +15,33 @@ class DashboardScreen extends Component {
 		this.setState({ email, displayName });
 	}
 
-	signOutUser = () => {
-		firebase.auth().signOut();
-	};
+	// signOutUser = () => {
+	// 	firebase.auth().signOut();
+	// };
 
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Hi {this.state.displayName}!</Text>
+				{/* <Text style={styles.dashboard_welcome}>Hi {this.state.displayName}!</Text> */}
 
-				<TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
+				{/* <TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
 					<Text>Logout</Text>
+				</TouchableOpacity> */}
+
+				<TouchableOpacity style={styles.featured_group}>
+					<View style={styles.feature_card}>
+						<Text style={styles.ad}>Featured</Text>
+						<Text>FormUp</Text>
+						<Text>Required React native</Text>
+
+						<Text>Yang Li | 1 Hour </Text>
+					</View>
 				</TouchableOpacity>
+
+				<View style={styles.rec_group}>
+					<Text>Recommendations</Text>
+				</View>
+				
 			</View>
 		);
 	}
@@ -37,8 +52,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  featured_group: {
+	flex: 1,
+	width: '100%',
+	height: '100%',
+  },
+  feature_card: {
+	top: 200,
+	left: 100
+  },
+  ad: {
+	width: '18%',
+	padding: 5,
+	borderWidth: 1,
+	borderColor: '#000000',
+	borderRadius: 15
+  },
+  rec_group: {
+	flex: 1
+  }
+
 });
 
 export default DashboardScreen
